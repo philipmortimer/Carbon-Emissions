8fdc6eba2947e78fb377537eb2268bcfefb7502c
elliot-mb <elliotbuckingham14@gmail.com>
2022-10-11 17:56:10 +0100

    added-sass-dependancy

--- frontend/route-zero-enterprise/src/App.js
+++ frontend/route-zero-enterprise/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import './App.scss';
+//import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
