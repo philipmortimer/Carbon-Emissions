fda9c48023b3a6b0d2b560fde8a0ca3d602e0c1e
elliot-mb <elliotbuckingham14@gmail.com>
2022-10-25 12:35:58 +0100

    added page folders and router

--- frontend/route-zero-enterprise/src/App.js
+++ frontend/route-zero-enterprise/src/App.js
@@ -7,12 +7,28 @@ import {
   Navigate
 } from "react-router-dom";
 
+import {Home} from "./pages/Home/Home.js";
+
 //import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
       <h1>Route Zero Carbon Tracking for Business</h1>
+      <Router className="router">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
