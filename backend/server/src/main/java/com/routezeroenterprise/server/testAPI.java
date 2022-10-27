0eb7c39104212800542d254a695f480aa2e116a5
Badr Makhdoom <77981639+BadrTM@users.noreply.github.com>
2022-10-27 15:59:46 +0100

    Removed line 5
    Contained false info

--- backend/server/src/main/java/com/routezeroenterprise/server/testAPI.java
+++ backend/server/src/main/java/com/routezeroenterprise/server/testAPI.java
@@ -2,7 +2,6 @@ package com.routezeroenterprise.server;
 
 public class testAPI {
 
-    // Variables here are what will be returned in JSON format by the server
     private final String content;
 
     public testAPI(String content) {
