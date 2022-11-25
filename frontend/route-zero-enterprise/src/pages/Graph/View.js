bfaf00d7d7c4658a7e053aeecda80792f20020cd
camillemolina1 <hy21734@bristol.ac.uk>
2022-11-25 16:45:27 +0000

    created chart component

--- frontend/route-zero-enterprise/src/pages/Graph/View.js
+++ frontend/route-zero-enterprise/src/pages/Graph/View.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {Container} from "react-bootstrap";
 
 import {readText} from '../../helpers/file_reader.js';
+import {Chart} from '../../components/Chart/Chart.js';
 
 import "./View.scss";
 
@@ -14,6 +15,13 @@ export const View = (props) => {
     return(<>
             <h1>Visualisation</h1>
             <p>{JSON.stringify(props.response)}</p>
+            <div>
+                <Chart/>
+                <Chart/>
+                <Chart/>
+                <Chart/>
+            </div>
+
         </>
     )
 }
