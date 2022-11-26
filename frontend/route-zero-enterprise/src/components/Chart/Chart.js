68c84abfa4e6a887c3706b9ea98543091841aa1a
camillemolina1 <hy21734@bristol.ac.uk>
2022-11-26 11:18:09 +0000

    starting styling page and inserting static graphs

--- frontend/route-zero-enterprise/src/components/Chart/Chart.js
+++ frontend/route-zero-enterprise/src/components/Chart/Chart.js
@@ -1,7 +1,30 @@
 import React from "react";
+import CanvasJSReact from '../../canvasjs.react';
 
 export const Chart = () => {
+
+    const options = {
+        axisY: {
+            title: "Journeys",
+        },
+        axisX:{
+            title: "transport methods",
+        },
+        data: [{
+            type: "column",
+            dataPoints: [
+                {label: "Car", y: 60},
+                {label: "Bus", y: 10},
+                {label: "Plane", y: 100},
+                {label: "Train", y: 40},
+            ]
+        }]
+    }
+
     return (
-        <p>"Chart"</p>
+        <div>
+            <CanvasJSReact.CanvasJSChart options = {options}
+            />
+        </div>
     )
 }
--- frontend/route-zero-enterprise/src/pages/Graph/View.js
+++ frontend/route-zero-enterprise/src/pages/Graph/View.js
@@ -15,11 +15,25 @@ export const View = (props) => {
     return(<>
             <h1>Visualisation</h1>
             <p>{JSON.stringify(props.response)}</p>
-            <div>
-                <Chart/>
-                <Chart/>
-                <Chart/>
-                <Chart/>
+            <div className="center-grid">
+                <div className="cell">
+                    <h2>Before</h2>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                </div>
+                <div className="cell">
+                    <h2>After</h2>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                </div>
             </div>
 
         </>
