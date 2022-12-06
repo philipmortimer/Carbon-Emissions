adb5fa0d973fa77c3e17b458ca5a8c7b9bc52cc4
elliot-mb <elliotbuckingham14@gmail.com>
2022-12-06 18:26:53 +0000

    repaired CORS and added deploy toggle

--- frontend/route-zero-enterprise/src/data/backend.js
+++ frontend/route-zero-enterprise/src/data/backend.js
@@ -1,5 +1,15 @@
-export const params = {
+const deployed = true;
+
+const deploy = {
     ip: "https://application-e8.vzjfxzf7sdt.eu-gb.codeengine.appdomain.cloud",
     port: 443,
     endpoint: "/get_predictions" 
+}
+
+const develop = {
+    ip: "http://localhost",
+    port: 8080,
+    endpoint: "/get_predictions" 
 };
+
+export const exposedEndpoints = deployed ? deploy : develop;
