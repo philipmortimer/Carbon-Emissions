6f1acf0f4defa674aaed99a52f53fb7d66f05682
Philip Mortimer <philipmortimer99@aol.com>
2023-02-16 23:58:52 +0000

    Changed dev tests for CI

--- backend/server/src/main/java/com/routezeroenterprise/server/ServerApplication.java
+++ backend/server/src/main/java/com/routezeroenterprise/server/ServerApplication.java
@@ -12,7 +12,7 @@ public class ServerApplication {
 
 	/**
 	 * Main method used to run server.
-	 * @param args Default system arguments array.
+	 * @param args Default system arguments vector.
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
