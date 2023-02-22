57c2fcbac1caed2b0d14617bf383403e805d24c5
BadrOSX <BadrTM@hotmail.com>
2023-02-22 02:31:21 +0000

    getPreditionAPITest Done

--- backend/server/src/test/java/com/routezeroenterprise/server/APIControllerTest.java
+++ backend/server/src/test/java/com/routezeroenterprise/server/APIControllerTest.java
@@ -1,37 +1,49 @@
 package com.routezeroenterprise.server;
 
+import static com.routezeroenterprise.server.ServerCSVTests.loadTextFileTest;
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(APIController.class)
 public class APIControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+	public void getPredictionAPITest() throws Exception {
+		MediaType mediaType = MediaType.parseMediaType("text/csv");
+		String CSV_VALID = loadTextFileTest("src/main/resources/TestCSV/VALID.csv");
+		String responseObject = "$.predictions[0].currentCarbonKgCo2e";
+		String expectedObjectValue = "7.952942888820001";
+
+		this.mvc.perform(MockMvcRequestBuilders
+						.post("/get_predictions")
+						.contentType(mediaType)
+						.content(CSV_VALID))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("{\"response\":\"Hello, World\"}")));
+				.andExpect(jsonPath(responseObject).value(expectedObjectValue));
 	}
 
-    @Test
+	@Test
 	public void getProperties() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/properties").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("{\"emissionsEndpoint\":\"https://predict-qzzieui6kq-ew.a.run.app\",\"frontendAddress\":\"localhost:3000\"}")));
 	}
-
 }
