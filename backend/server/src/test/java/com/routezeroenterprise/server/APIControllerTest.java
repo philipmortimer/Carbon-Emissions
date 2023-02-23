0bca0ce6730b2000c153d685b187616a2039b333
camillemolina1 <hy21734@bristol.ac.uk>
2023-02-23 14:45:43 +0000

    tests for invalid files and timeouts

--- backend/server/src/test/java/com/routezeroenterprise/server/APIControllerTest.java
+++ backend/server/src/test/java/com/routezeroenterprise/server/APIControllerTest.java
@@ -7,6 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.RepeatedTest;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(APIController.class)
@@ -37,4 +41,33 @@ public class APIControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath(responseObject).value(expectedObjectValue));
 	}
+
+	@Test
+	public void getPredictionAPITestInvalid() throws Exception {
+		MediaType mediaType = MediaType.parseMediaType("text/csv");
+		String CSV_INVALID = loadTextFileTest("src/main/resources/TestCSV/INVALID_TRANSPORT.csv");
+
+		this.mvc.perform(MockMvcRequestBuilders
+						.post("/get_predictions")
+						.contentType(mediaType)
+						.content(CSV_INVALID))
+				.andExpect(status().isOk())
+				.andExpect(content().string("{\"error\": \"Line 8 contains an invalid transport type.  Transport type 'airplane' is invalid.\"}"));
+
+	}
+
+	@Test
+	@RepeatedTest(10)
+	@Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
+	public void performanceTest() throws Exception {
+		MediaType mediaType = MediaType.parseMediaType("text/csv");
+		String CSV_VALID = loadTextFileTest("src/main/resources/TestCSV/VALID.csv");
+
+		this.mvc.perform(MockMvcRequestBuilders
+						.post("/get_predictions")
+						.contentType(mediaType)
+						.content(CSV_VALID))
+				.andExpect(status().isOk());
+	}
+
 }
