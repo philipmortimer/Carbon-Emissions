1af184430ae178d1138e458cd1a912c1a9b2bcd0
Philip Mortimer <philipmortimer99@aol.com>
2023-02-28 12:43:44 +0000

    Checked state of buttons

--- frontend/route-zero-enterprise/src/__tests__/Home/home.test.js
+++ frontend/route-zero-enterprise/src/__tests__/Home/home.test.js
@@ -34,6 +34,8 @@ describe("Home page aesthetics", () => {
         render(getHomeTestComponent(getDefaultPropsHome()));
         // Checks that the default state of buttons being enabled / disbaled is correct
         expect(screen.getAllByText("CSV Schema")[0].disabled).toBe(false);
+        expect(screen.getAllByText("Upload")[0].disabled).toBe(false);
+        expect(screen.getAllByText("See predictions")[0].disabled).toBe(true);
     })
 });
 
