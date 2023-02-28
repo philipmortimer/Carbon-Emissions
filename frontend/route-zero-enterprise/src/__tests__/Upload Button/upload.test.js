b0b23a33df5d9a2b57b909310619a506383e27a9
Philip Mortimer <philipmortimer99@aol.com>
2023-02-28 16:29:00 +0000

    Added partial framework for upload component

---
+++ frontend/route-zero-enterprise/src/__tests__/Upload Button/upload.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { Upload } from '../../components/Upload/Upload';
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from "react-router-dom";
+import '@testing-library/jest-dom'
+
+describe("Tests upload component", () => {
+    test("Tests upload component when no file has been uploaded", () => {
+        
+    });
+});
