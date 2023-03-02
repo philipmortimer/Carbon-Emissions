ca42804e80a52c65128016505fda8a34e32cc02d
Philip Mortimer <philipmortimer99@aol.com>
2023-03-02 16:28:04 +0000

    Completed hero test

--- frontend/route-zero-enterprise/src/__tests__/Hero/hero.test.js
+++ frontend/route-zero-enterprise/src/__tests__/Hero/hero.test.js
@@ -13,6 +13,9 @@ describe("Tests aesthetics of header", () => {
         render(getHero(null));
         expect(screen.queryByText("Upload")).toBeInTheDocument();
         expect(screen.queryByText("View")).toBeInTheDocument();
+        // Test default page layout is correct
+        expect(screen.queryByText("up page")).toBeInTheDocument();
+        expect(screen.queryByText("view page")).not.toBeInTheDocument();
     });
     test("Default page for hero test is upload", () => {
         render(getHero(null));
@@ -27,6 +30,44 @@ describe("Tests aesthetics of header", () => {
         });
         expect(screen.queryByText("up page")).toBeInTheDocument();
         expect(screen.queryByText("view page")).not.toBeInTheDocument();
+    });
+    test("API response enables view tab", async () => {
+        render(getHero("sample_response"));
+        // Clicks view button
+        await waitFor(() => {
+            userEvent.click(screen.getByText("View"));
+        });
+        expect(screen.queryByText("up page")).not.toBeInTheDocument();
+        expect(screen.queryByText("view page")).toBeInTheDocument();
+    });
+    test("Can navigate back to upload tab after viewing API response", async () => {
+        render(getHero("sample_response"));
+        // Clicks view button and then goes back to upload tab
+        await waitFor(() => {
+            userEvent.click(screen.getByText("View"));
+            userEvent.click(screen.getByText("Upload"));
+        });
+        expect(screen.queryByText("up page")).toBeInTheDocument();
+        expect(screen.queryByText("view page")).not.toBeInTheDocument();
+    });
+    test("Upload button takes user to upload page when already on upload page" , async () => {
+        render(getHero("sample_response"));
+        // Clicks upload button
+        await waitFor(() => {
+            userEvent.click(screen.getByText("Upload"));
+        });
+        expect(screen.queryByText("up page")).toBeInTheDocument();
+        expect(screen.queryByText("view page")).not.toBeInTheDocument();
+    });
+    test("Pressing view button in view tab causes user to stay there", async () => {
+        render(getHero("sample_response"));
+        // Clicks view button twice
+        await waitFor(() => {
+            userEvent.click(screen.getByText("View"));
+            userEvent.click(screen.getByText("View"));
+        });
+        expect(screen.queryByText("up page")).not.toBeInTheDocument();
+        expect(screen.queryByText("view page")).toBeInTheDocument();
     });
 });
 
