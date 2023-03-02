9991fa7c85ecc2a36be970d3aab80b99df28e62a
Philip Mortimer <philipmortimer99@aol.com>
2023-03-02 14:13:19 +0000

    More modal tests

---
+++ frontend/route-zero-enterprise/src/__tests__/Invalid File Modal/modal.test.js
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import { InvalidFileModal } from '../../components/InvalidFileModal/InvalidFileModal';
+
+describe("Aesthetic tests invalid file modal", () => {
+    test("Test modal displays correctly for chosen error message", async () => {
+        const msg = "testMessage here";
+        render(<InvalidFileModal show={true} msg={msg} />);
+        // Test that invalid file modal appears
+        expect(screen.getByText("Invalid CSV File")).toBeInTheDocument();
+        expect(screen.getByText("File Incorretly Formatted")).toBeInTheDocument();
+        expect(screen.getByText("The CSV file chosen is invalid as it does not meet the CSV schema." +
+            " Please review the CSV schema and upload a valid file." +
+            " An error message is provided below to help identify the issue.")).toBeInTheDocument();
+        expect(screen.getByText(msg)).toBeInTheDocument();
+        expect(screen.getByText("Close")).toBeInTheDocument();
+    });
+});
