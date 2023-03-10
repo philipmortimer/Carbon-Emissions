85d519f67b3c14d315a0ec2ac108eb393f9bf777
elliot-mb <elliotbuckingham14@gmail.com>
2023-03-10 19:55:24 +0000

    Chart prompts not enough data

--- frontend/route-zero-enterprise/src/components/Chart/Chart.js
+++ frontend/route-zero-enterprise/src/components/Chart/Chart.js
@@ -60,7 +60,12 @@ export const BarChart = ({chartId,  header, bars}) => {
     // 01/26/23 chardID and header were added to mitigate 'react-hooks/exhaustive-deps' warning
 
     return (
-        <canvas id={chartId}/>
-    )
+        <>
+            <span className="chart-overlay">{ bars.length === 0
+            ? "Sorry, not enough data"
+            : "" }</span>
+            <canvas id={chartId}/>
+        </>
+    );
 
 }
