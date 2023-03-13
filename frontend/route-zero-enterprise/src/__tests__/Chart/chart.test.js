5958b7bfd499c3b641181074e27f9b7258e372cd
Philip Mortimer <philipmortimer99@aol.com>
2023-03-13 16:38:30 +0000

    Completed tally list tests

--- frontend/route-zero-enterprise/src/__tests__/Chart/chart.test.js
+++ frontend/route-zero-enterprise/src/__tests__/Chart/chart.test.js
@@ -9,4 +9,28 @@ describe("Tally List Tests", () => {
         const set = tallyList(list)
         expect(JSON.stringify(set) === JSON.stringify({})).toBe(true);
     })
+    test("Single element list returns correct tally", () => {
+        const list = ['train']
+        const map = tallyList(list)
+        const expectedMap = {'train': 1}
+        expect(JSON.stringify(map) === JSON.stringify(expectedMap)).toBe(true);        
+    })
+    test("Multiple instances of single type of instance sum up", () => {
+        const list = ['train', 'train', 'train', 'train']
+        const map = tallyList(list)
+        const expectedMap = {'train': 4}
+        expect(JSON.stringify(map) === JSON.stringify(expectedMap)).toBe(true);           
+    })
+    test("Multiple types are all correctly tallied together", () => {
+        const list = ['car', 'train', 'train', 'car', 'foot', 'bike', 'bike', 'train', 'train', 'chopper']
+        const map = tallyList(list)
+        const expectedMap = {
+            'car': 2,
+            'train': 4,
+            'foot': 1,
+            'bike': 2,
+            'chopper': 1
+        }
+        expect(JSON.stringify(map) === JSON.stringify(expectedMap)).toBe(true);             
+    })
 })
