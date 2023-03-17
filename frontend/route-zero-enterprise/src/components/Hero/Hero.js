6f107514ac605c34293b0edeb689576c7aa2e239
Smaranda Tasmoc <fo21366@bristol.ac.uk>
2023-03-17 11:54:36 +0000

    add link to csv schema page from navbar

--- frontend/route-zero-enterprise/src/components/Hero/Hero.js
+++ frontend/route-zero-enterprise/src/components/Hero/Hero.js
@@ -15,8 +15,10 @@ export const Hero = (props) => {
         <Container fluid>
           <Navbar.Brand className='navbar-brand' href='https://routezero.world/'><Logo />{/* RouteZero */}</Navbar.Brand>
           <Nav className='me-auto'>
+            <Link className='nav-link' to='/schema'>CSV Schema</Link>
             <Link className='nav-link' to='/'>Upload</Link>
             <Link className='nav-link' id={'view-link' + (props.response === null ? '-disabled' : '')} to={props.response === null ? '/' : '/view'}>View</Link>
+            
           </Nav>
         </Container>
       </Navbar>
