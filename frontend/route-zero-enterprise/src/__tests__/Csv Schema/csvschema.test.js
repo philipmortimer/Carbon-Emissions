3eab0de1af8d07fb7fa7fedeef018ec1bba4b0c5
Philip Mortimer <philipmortimer99@aol.com>
2023-03-23 21:16:21 +0000

    New schema tests

--- frontend/route-zero-enterprise/src/__tests__/Csv Schema/csvschema.test.js
+++ frontend/route-zero-enterprise/src/__tests__/Csv Schema/csvschema.test.js
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import { PromptSchemaCSV } from '../../components/Prompts/Prompts'
+import { CSVSchema } from '../../pages/CSVSchema/CSVSchema'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import {
@@ -8,7 +9,7 @@ import {
   Route
 } from 'react-router-dom'
 
-describe('CSV Schema Component Tests', () => {
+describe('PromptSchemaCSV Component Tests', () => {
   test('CSV Schema button exists', () => {
     render(
       <>
@@ -36,5 +37,42 @@ describe('CSV Schema Component Tests', () => {
     // Clicks schema button
     userEvent.click(screen.getByText("CSV Schema"))
     expect(screen.queryByText("gone to schema")).toBeInTheDocument()
+  })
+})
+
+describe("CSVSchema component tests", () => {
+  test("Aesthetics of csv schema", () => {
+    render(<CSVSchema />)
+    // Tests general aesthetics of component
+    expect(screen.getByText("CSV Schema")).toBeInTheDocument()
+    expect(screen.getByText("Line format")).toBeInTheDocument()
+    expect(screen.getByText("Please format each line of your CSV as follows:")).toBeInTheDocument()
+    expect(screen.getByText("origin,destination,distanceKm,departureTime,arrivalTime,transport")).toBeInTheDocument()
+    expect(screen.getByText("Download example")).toBeInTheDocument()
+    expect(screen.getByText("Download")).toBeInTheDocument()
+    expect(screen.getByText("Origin & Destination")).toBeInTheDocument()
+    expect(screen.getByText("These are the name of the source and destination of your travel journey. Please include these names, but you could also opt to omit them. For more information, please refer to the example above.")).toBeInTheDocument()
+    expect(screen.getByText("Distance")).toBeInTheDocument()
+    expect(screen.getByText("This is the number of kilometers you travelled between source and destination. This information could easily be found through a map application such as Google Maps.")).toBeInTheDocument()
+    expect(screen.getByText("Arrival & Departure time")).toBeInTheDocument()
+    expect(screen.getByText("Please include the time in the following format yyyy-mm-ddThh:mm:ss.000Z, where yyyy stands for year, mm stands for month, dd stands for day, hh stands for hour, mm stands for minutes and ss stands for seconds, e.g. 2022-10-14T17:48:00.000Z.")).toBeInTheDocument()
+    expect(screen.getByText("Transport")).toBeInTheDocument()
+    expect(screen.getByText("To include the type of transportation used, please pick one from the following dictionary and use the exact spelling seen bellow:")).toBeInTheDocument()
+    expect(screen.getByText("foot")).toBeInTheDocument()
+    expect(screen.getByText("dieselCar")).toBeInTheDocument()
+    expect(screen.getByText("bus")).toBeInTheDocument()
+    expect(screen.getByText("tram")).toBeInTheDocument()
+    expect(screen.getByText("bike")).toBeInTheDocument()
+    expect(screen.getByText("hybridCar")).toBeInTheDocument()
+    expect(screen.getByText("coach")).toBeInTheDocument()
+    expect(screen.getByText("subway")).toBeInTheDocument()
+    expect(screen.getByText("electricScooter")).toBeInTheDocument()
+    expect(screen.getByText("electricCar")).toBeInTheDocument()
+    expect(screen.getByText("eurostar")).toBeInTheDocument()
+    expect(screen.getByText("flight")).toBeInTheDocument()
+    expect(screen.getByText("petrolCar")).toBeInTheDocument()
+    expect(screen.getByText("taxi")).toBeInTheDocument()
+    expect(screen.getByText("lightRail")).toBeInTheDocument()
+    expect(screen.getByText("lightRail")).toBeInTheDocument()
   })
 })
