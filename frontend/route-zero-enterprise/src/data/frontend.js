4bf83650aee1bd3655e20ed76946d7bed94c4db0
elliot-mb <elliotbuckingham14@gmail.com>
2023-03-23 15:13:41 +0000

    move files around

--- frontend/route-zero-enterprise/src/data/frontend.js
+++
@@ -1,8 +0,0 @@
-export const params = {
-  ip: 'localhost',
-  port: 3000
-}
-
-export const client = {
-  request_id: '' // store in localstorage? this relates to updating the policy & re-requesting graphs on visualise page
-}
