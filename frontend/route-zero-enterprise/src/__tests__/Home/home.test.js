50448dfd0693cd99ca62d425c3c59dc6ad7a774b
camillemolina1 <hy21734@bristol.ac.uk>
2023-03-30 21:03:14 +0100

    modified frontend tests

--- frontend/route-zero-enterprise/src/__tests__/Home/home.test.js
+++ frontend/route-zero-enterprise/src/__tests__/Home/home.test.js
@@ -32,6 +32,7 @@ describe('Home page aesthetics', () => {
     expect(screen.getAllByText('Simply upload your travel expense or milage data as a CSV or JSON file').length).toBe(1)
     expect(screen.getAllByText('Please select a file').length).toBe(1)
     expect(screen.getAllByText('CSV Schema').length).toBe(1)
+    expect(screen.getAllByText('JSON Schema').length).toBe(1)
     expect(screen.getAllByText('Upload').length).toBe(1)
     expect(screen.getAllByText('See predictions').length).toBe(1)
   })
@@ -39,6 +40,7 @@ describe('Home page aesthetics', () => {
     render(getHomeTestComponent(getDefaultPropsHome()))
     // Checks that the default state of buttons being enabled / disbaled is correct
     expect(screen.getAllByText('CSV Schema')[0].disabled).toBe(false)
+    expect(screen.getAllByText('JSON Schema')[0].disabled).toBe(false)
     expect(screen.getAllByText('Upload')[0].disabled).toBe(false)
     expect(screen.getAllByText('See predictions')[0].disabled).toBe(true)
   })
